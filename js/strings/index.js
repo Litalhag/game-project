d8d3292688f8763d1120fe7f59d651a7022a5574
MariaPinchasi <135972701+MariaPinchasi@users.noreply.github.com>
2023-07-18 19:48:46 +0300

    allExe

--- js/strings/index.js
+++ js/strings/index.js
@@ -44,3 +44,48 @@ function getGameAcronym(title){
 console.log(getGameAcronym("Epic Fantasy Battle"));
 
 // 6
+function getCharacterDetails(character){
+    let str='';
+    for (let prop in character){
+        str+=`${prop}: ${character[prop]} `;
+    }
+    return str;
+}
+console.log(getCharacterDetails({ name: "Iron Warrior", class: "Warrior", level: 10 }));
+// 7
+function createSlug(location){
+    return location.toLowerCase().split(' ').join('-');
+}
+console.log(createSlug("Dark Forest"));
+// 8
+function trimDescription(description, maxLength){
+    const arr=[...description];
+    const newLength=arr.length-maxLength;
+    let str='';
+    for (let i=0; i<=newLength; i++){
+        str+=arr[i];
+    }
+    return str;
+}
+console.log(trimDescription("Retrieve the sacred artifact from the dragon's lair. But beware,the dragon is cunning and powerful."
+, 50));
+// 9
+function splitMonsters(monsters){
+    const arr= monsters.split(', ');
+    return arr;
+}
+console.log (splitMonsters("Goblin, Orc, Dragon"));
+
+// 10
+function decodeSecret(secret){
+    const arr=[...secret];
+    const mapping = { '1': 'i', '2': 'b', '3': 'e', '4': 'a', '5': 'e' };
+    const newArr=arr.map((char)=>{
+        if (mapping[char]){
+            return mapping [char];
+        }
+        return char;
+    });
+    return newArr.join("");
+}
+console.log(decodeSecret("Th3 s3cr3t l1r3 1s und3r th3 br1dg3"));
