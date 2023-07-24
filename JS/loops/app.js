3802762584a724334b83651b0412a5a790cbeb69
Mony Baruch <monybaruch@gmail.com>
2023-07-24 20:31:38 +0300

    adding solutions to 4~7

--- JS/loops/app.js
+++ JS/loops/app.js
@@ -62,3 +62,70 @@ const findEnemeiesByHp = (enemies, healthThreshold) => {
   return filteredEnemeiesByHp;
 };
 console.log(findEnemeiesByHp(enemies, 100));
+// 1 more way with Destructuring credit to Maria
+const questsByExperience = (questsArr) => {
+  for (let i = 0; i < questsArr.length; i++) {
+    for (let j = 0; j < questsArr.length - 1 - i; j++) {
+      if (questsArr[j].experience > questsArr[j + 1].experience) {
+        [questsArr[j], questsArr[j + 1]] = [questsArr[j + 1], questsArr[j]];
+      }
+    }
+  }
+  return questsArr;
+};
+// 5.
+
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+const sumMatrix = (matrix) => {
+  let sum = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      sum += matrix[i][j];
+    }
+  }
+  return sum;
+};
+
+console.log(sumMatrix(matrix));
+
+// 6.
+
+const matrix2 = [
+  [3, 222, 2],
+  [100, 1, 7],
+  [55, 89, 6],
+];
+
+const maxNumInMatrix = (arr) => {
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] > max) {
+        max = arr[i][j];
+      }
+    }
+  }
+  return max;
+};
+
+console.log(maxNumInMatrix(matrix2));
+
+// 7.
+const sumEvenMatrix = (matrix) => {
+  let sum = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[j][i] % 2 === 0) {
+        sum++;
+      }
+    }
+  }
+  return sum;
+};
+
+console.log(sumEvenMatrix(matrix));
