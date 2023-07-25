be084f2f8080b6831380f2e860c02f86d9bc254a
Ori Baram <obrm770@gmail.com>
2023-07-25 12:44:18 +0300

    Code review

--- js/loop/extra/index.js
+++ js/loop/extra/index.js
@@ -1,17 +1,19 @@
-// 1
-const movePlayer=(gameWorldGrid,currentPosition,direction)=>{
-    if (direction='north'&& currentPosition.x>0) {
-        currentPosition.x--;
+const movePlayer = (gameWorldGrid, currentPosition, direction) => {
+    // CR - Use the triple equals (===) for comparison, not the assignment operator (=)
+    if (direction = 'north' && currentPosition.x > 0) { // CR - This should be if (direction === 'north' && currentPosition.x > 0)
+        currentPosition.x--; // CR - This is correct for moving north
     }
-    if (direction='south'&& currentPosition.x<gameWorldGrid.length-1) {
-        currentPosition.x++;
+    // CR - Use the triple equals (===) for comparison, not the assignment operator (=)
+    if (direction = 'south' && currentPosition.x < gameWorldGrid.length - 1) { //CR - This should be if (direction === 'south' && currentPosition.x < gameWorldGrid.length - 1)
+        currentPosition.x++; // CR - This is correct for moving south
     }
-    if (direction='east'&& currentPosition.y>0) {
-        currentPosition.y--;
+    // CR - Use the triple equals (===) for comparison, not the assignment operator (=)
+    if (direction = 'east' && currentPosition.y > 0) { // CR - This should be if (direction === 'east' && currentPosition.y < gameWorldGrid[0].length - 1)
+        currentPosition.y--; // CR - This should be currentPosition.y++ for moving east
     }
-    if (direction='west'&& currentPosition.y>gameWorldGrid[currentPosition.x].length-1) {
-        currentPosition.y++;
+    // CR - Use the triple equals (===) for comparison, not the assignment operator (=)
+    if (direction = 'west' && currentPosition.y > gameWorldGrid[currentPosition.x].length - 1) { // CR - This should be if (direction === 'west' && currentPosition.y > 0)
+        currentPosition.y++; // CR - This should be currentPosition.y-- for moving west
     }
     return currentPosition;
-
 }
