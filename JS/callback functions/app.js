646ec2bcf95c1c0dc5eac4ab3612a6cf60c83e75
Mony Baruch <monybaruch@gmail.com>
2023-07-31 21:24:22 +0300

    adding cb task number 2

--- JS/callback functions/app.js
+++ JS/callback functions/app.js
@@ -17,9 +17,9 @@ function processEnemies(enemies, cb) {
   }
   for (let enemy of enemies) {
     if (enemy.hasOwnProperty("enemyName") && enemy.hasOwnProperty("health")) {
-      console.log("shit");
+      console.log("it works");
     } else {
-      throw new Error("Enemy the ID is not in the right structure");
+      throw new Error(`Enemy the ID ${enemy.id} is not in the right structure`);
     }
   }
   let newEnemies = JSON.parse(JSON.stringify(enemies));
@@ -29,3 +29,40 @@ function processEnemies(enemies, cb) {
 }
 
 console.log(processEnemies(enemies, addHealth));
+
+//2
+
+const quests = [
+  { id: 1, questName: "collect mushrooms", experience: 100 },
+  { id: 2, questName: "buy items", experience: 90 },
+  { id: 3, questName: "collect apples", experience: 80 },
+];
+
+function addExp(newQuests) {
+  for (let quest of newQuests) {
+    quest.experience += 100;
+  }
+}
+
+function processQuests(quests, addExp) {
+  if (Array.isArray(quests)) {
+  } else {
+    throw new Error("Expected an array of quests");
+  }
+  for (let quest of quests) {
+    if (
+      quest.hasOwnProperty("questName") &&
+      quest.hasOwnProperty("experience")
+    ) {
+      console.log("it works");
+    } else {
+      throw new Error(`Enemy the ID ${quest.id} is not in the right structure`);
+    }
+  }
+  let newQuests = JSON.parse(JSON.stringify(quests));
+  addExp(newQuests);
+
+  return newQuests;
+}
+
+console.log(processQuests(quests, addExp));
