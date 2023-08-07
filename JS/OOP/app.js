a02c9ca4329f2193c6c7382f0eda3e9f2665eee5
Mony Baruch <monybaruch@gmail.com>
2023-08-07 20:46:29 +0300

    adding 2 more sub classes to the main character class

--- JS/OOP/app.js
+++ JS/OOP/app.js
@@ -1,13 +1,14 @@
+// part 1
 class Character {
-  constructor(name, health, strenght, inventory = []) {
+  constructor(name, health, strength) {
     this.name = name;
     this.health = health;
-    this.strenght = strenght;
-    this.inventory = inventory;
+    this.strength = strength;
+    this.inventory = [];
   }
 
   attack(target) {
-    target.health -= this.strenght;
+    target.health -= this.strength;
     return `${this.name} attacked ${target.name} successfully and his remaining hp is ${target.health}`;
   }
 
@@ -25,7 +26,7 @@ class Character {
 
   displayCharacter() {
     return `The character is ${this.name} he has ${this.health} hp , ${
-      this.strenght
+      this.strength
     } attack power and has ${this.inventory.join(" and ")} in his inventory`;
   }
 }
@@ -48,3 +49,27 @@ console.log(cloud);
 console.log(cloud.removeItem("healing potion"));
 console.log(cloud);
 console.log(cloud.displayCharacter());
+
+//part 2 - extending the character class and creating 2 new classes Player and Enemy Subclasses
+
+class Player extends Character {
+  constructor(name, health, strength, level) {
+    super(name, health, strength);
+    this.level = level;
+  }
+}
+
+const tifa = new Player("tifa lockhart", 100, 65, 99);
+
+console.log(tifa);
+
+class Enemy extends Character {
+  constructor(name, health, strength, type) {
+    super(name, health, strength);
+    this.type = type;
+  }
+}
+
+const bahamut = new Enemy("Bahamut Zero", 100, 99, "mystical dragon");
+
+console.log(bahamut);
