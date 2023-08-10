be5bd64ed03b06eb188aee80e22d8b449a724787
Mony Baruch <monybaruch@gmail.com>
2023-08-10 13:24:20 +0300

    finishing 1~4 in Exercise 1

--- DOM/Basic DOM Manipulation/1 - DOM Traversal and Manipulation/app.js
+++ DOM/Basic DOM Manipulation/1 - DOM Traversal and Manipulation/app.js
@@ -1,3 +1,25 @@
-//2
+//2 Selecting a Specific List Item
 const myLi = document.querySelector("li.start-here");
 myLi.textContent = "title 2";
+
+//3 Changing Text Content
+
+const myLiNextSibling = myLi.nextElementSibling;
+console.log(myLiNextSibling);
+
+//4 Adding a New Sub Title
+
+const myUl = myLiNextSibling.firstElementChild;
+
+console.log(myUl);
+
+newSubTitle = document.createElement("li");
+
+const subTitleText = document.createTextNode("sub title 4");
+
+console.log(subTitleText);
+
+newSubTitle.appendChild(subTitleText);
+console.log(newSubTitle);
+
+myUl.appendChild(newSubTitle);
