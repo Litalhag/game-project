b388585ac96d25ad7f734ff48baacf09b693c990
Mony Baruch <monybaruch@gmail.com>
2023-08-10 14:04:53 +0300

    finishing Exercise 1

--- DOM/Basic DOM Manipulation/1 - DOM Traversal and Manipulation/app.js
+++ DOM/Basic DOM Manipulation/1 - DOM Traversal and Manipulation/app.js
@@ -23,3 +23,30 @@ newSubTitle.appendChild(subTitleText);
 console.log(newSubTitle);
 
 myUl.appendChild(newSubTitle);
+
+// 5 Removing the Last Title
+
+const myLiParent = myLi.parentElement;
+
+console.log(myLiParent);
+
+myLiParent.lastElementChild.remove();
+
+//6 Updating the Document's Title
+
+const head = document.querySelector("head");
+
+console.log(head);
+
+const title = head.lastElementChild;
+// const title = document.head.querySelector('title') 1 more way to store the title
+console.log(title);
+
+title.textContent = "DOM noob!";
+
+const div = myLiParent.nextElementSibling;
+
+console.log(div);
+
+div.textContent =
+  "Now I know how to traverse the DOM :D , but what i really want is to travel to south Korea and have some fun there :(";
