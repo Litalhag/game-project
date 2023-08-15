d22f23ec6a1d2589f787a6d3682813d734314d00
Litalhag <litalhag@gmail.com>
2023-08-15 14:09:27 +0300

    extra

--- DOM/Basic DOM Manipulation/extra/copy mulitple inputs/index.js
+++ DOM/Basic DOM Manipulation/extra/copy mulitple inputs/index.js
@@ -15,7 +15,49 @@ function generateCode() {
 
   return code;
 }
+
 const sms = generateCode();
 console.log(sms);
 
-// visCode.value = sms;
+visCode.value = sms;
+
+numList[0].focus();
+
+function fillCode(el) {
+  // make sure if entered alphanumeric character
+  if (/^[a-zA-Z0-9]$/.test(el.value)) {
+    let combinedCode = Array.from(numList)
+      .reduce((acc, input) => {
+        return acc + input.value;
+      }, "")
+      .toLocaleLowerCase();
+
+    // Assign the combined value to the hidden input
+    hidCode.value = combinedCode;
+    const currentIndex = Array.from(numList).indexOf(el);
+    if (currentIndex < numList.length - 1) {
+      numList[currentIndex + 1].focus();
+    }
+  } else {
+    el.value = "";
+    alert("Please enter an alphanumeric character.");
+  }
+}
+
+function checkSubmit() {
+  if (visCode.value === sms) {
+    form.submit();
+    return true;
+  } else {
+    alert("The entered code does not match. Please try again.");
+    return false;
+  }
+}
+
+function checkValue(el) {
+  if (/^[a-zA-Z0-9]$/.test(el.value)) {
+    el.classList.remove("error");
+  } else {
+    el.classList.add("error");
+  }
+}
