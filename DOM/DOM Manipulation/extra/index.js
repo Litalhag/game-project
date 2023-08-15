01d47fb13335f4c3e3fbb6bb3d24a15254c1ff9e
MariaPinchasi <135972701+MariaPinchasi@users.noreply.github.com>
2023-08-15 11:28:38 +0300

    extra

--- DOM/DOM Manipulation/extra/index.js
+++ DOM/DOM Manipulation/extra/index.js
@@ -1,8 +1,9 @@
 const form = document.querySelector("#form");
-const numList = document.querySelectorAll("input");
+const numList = form.querySelectorAll("input[type=text]");
+console.log(numList);
 const hidCode = document.querySelector("#hid-code");
 const visCode = document.querySelector("#vis-code");
-const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const sms = randomCode(chars);
 
 function randomCode(chars) {
@@ -12,12 +13,43 @@ function randomCode(chars) {
   return result;
 }
 visCode.value = sms;
-numList[1].focus();
+numList[0].focus();
 
-function fillCode(el) {
-  if (/^[a-zA-Z0-9]$/.test(el.value)) {
-    hidCode.value = Array.from(numList)
-      .reduce((acc, char) => acc + char, "")
-      .toLowerCase();
-  } else el = "";
+function fillCode() {
+  if (/^[a-zA-Z0-9]$/.test(this.value)) {
+    hidCode.value = Array.from(numList).reduce(
+      (acc, char) => acc + char.value,
+      ""
+    );
+    // .toLowerCase();
+    if (this.nextElementSibling) this.nextElementSibling.focus();
+  } else this.value = "";
 }
+function fillFromClipboard(event) {
+  console.log(event);
+  event.preventDefault();
+  const paste = (event.clipboardData || window.clipboardData).getData("text");
+  if (paste.length !== 6 || /^[a-zA-Z0-9]$/.test(paste)) return false;
+  else {
+    for (i = 0; i < 6; i++) {
+      numList[i].value = paste[i];
+    }
+  }
+}
+
+function checkAndSubmit() {
+  if (visCode.value === hidCode.value) {
+    form.submit();
+  } else return false;
+}
+function checkValue() {
+  if (/^[a-zA-Z0-9]$/.test(this.value)) this.classList.remove("error");
+  else this.classList.add("error");
+}
+
+numList.forEach((el) => {
+  el.addEventListener("keyup", fillCode);
+  el.addEventListener("keyup", checkValue);
+  el.addEventListener("paste", fillFromClipboard);
+  el.addEventListener("keyup", checkAndSubmit);
+});
